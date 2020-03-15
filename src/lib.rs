b75bfb67fdeca0456e966d2a2b1420ce4a24be82
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-03-15 23:07:16 +0000

    Initial project

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! # resec
+//!
+//! A library dedicated to reverse engineering techniques for the [SEC](https://examinations.ie).
+//!
+//! **Note**: This library can stop working at any time if a website change occurs!
