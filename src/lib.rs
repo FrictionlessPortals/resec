9ecb5c848db7a1b92b186a8d27d44e6dc9e7d921
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2020-03-16 18:49:50 +0000

    Add prelude

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,24 @@
 //!
 //! **Note**: This library can stop working at any time if a website change occurs!
 
-pub mod consts;
+mod consts;
 pub mod error;
 pub mod parser;
 pub mod stages;
+
+/// Convenience re-export of commonly used items.
+///
+/// # Usage:
+///
+/// ```
+/// use resec::prelude::*;
+/// ```
+pub mod prelude {
+    // SEC Prelude
+    pub use crate::{
+        consts::{EXAM_PAPER_YEARS, MARKING_SCHEME_YEARS},
+        error::SecError,
+        parser::*,
+        stages::StageBuilder,
+    };
+}
